d41ad145bcec3a77d478ab4f8450ad3a930014c3
Tony Blard <tonyblard55@gmail.com>
2022-11-21 18:07:01 +0100

    Remove std::cout

--- selection_sort/my_sort.hpp
+++ selection_sort/my_sort.hpp
@@ -6,7 +6,6 @@
 template<class IT>
 void my_selection_sort( IT begin, IT end)
 {
-    std::cout << "selection sort " << std::endl;
     IT min;
     IT next;
     for (IT i = begin; i != end; ++i)
@@ -15,14 +14,11 @@ void my_selection_sort( IT begin, IT end)
         next = i; 
         for (IT j = ++next; j != end; ++j)
         {
-            // std::cout << " *min= "<< *min << " *j = " << *j << std::endl;
             if (*j < *min)
             {
                 min = j;
             }
         }
-        // std::cout << " *min= "<< *min << " *i = " << *i << std::endl;
         std::swap(*min, *i);
-        // std::cout << " *min= "<< *min << " *i = " << *i << std::endl;
     }
 }
